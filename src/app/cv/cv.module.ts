5198e0cc542b4331c3fa7bbc40dd9ef3d6c3dc04
estudiante <estudiante@ISIS2603-372.sis.virtual.uniandes.edu.co>
2018-10-18 09:08:05 -0500

    cv-list

--- src/app/cv/cv.module.ts
+++ src/app/cv/cv.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CvListComponent } from './cv-list/cv-list.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [CvListComponent]
 })
 export class CvModule { }
