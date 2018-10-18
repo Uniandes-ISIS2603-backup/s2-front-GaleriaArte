f298c25b0f7f7e75fdf7c9786299733da8d9b3a7
sacostav <s.acostav@uniandes.edu.co>
2018-10-18 08:41:11 -0500

    artist module

---
+++ src/app/buyer/buyer.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BuyerListComponent } from './buyer-list/buyer-list.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [BuyerListComponent]
+})
+export class BuyerModule { }
